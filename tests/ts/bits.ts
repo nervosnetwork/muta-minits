3b7fdb4dbb6caaf8343c02dace31a8c443ae6c2b
mohanson <mohanson@outlook.com>
2019-08-15 14:55:44 +0800

    Fix tests

--- tests/ts/bits.ts
+++ tests/ts/bits.ts
@@ -3,8 +3,8 @@ function main(): number {
     a = a & 0x70; // 0x70
     a = a | 0x0f; // 0x7f
     a = ~a; // 0x80
-    // a = a ^ 0x77; // 0xf7
-    // a = a << 1; // 0xee
-    // a = a >> 2; // 0x3b
+    a = a ^ 0x77; // 0xf7
+    a = a << 1; // 0xee
+    a = a >> 2; // 0x3b
     return a;
 }
