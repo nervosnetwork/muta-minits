4a7bb87a4c093c17b80191c010d42d1028c6bb16
mohanson <mohanson@outlook.com>
2019-08-16 15:33:31 +0800

    Fix factorial

--- tests/ts/application/factorial.ts
+++ tests/ts/application/factorial.ts
@@ -3,9 +3,9 @@
 //
 // F(n) = n × (n − 1) × (n − 2) . . . × 3 × 2 × 1
 function factorial(n: number): number {
-    let s: number = 0;
-    for (let i: number = 0; i < n; i++) {
-        s = s + i;
+    let s: number = 1;
+    for (let i: number = 1; i <= n; i++) {
+        s = s * i;
     }
     return s;
 }
