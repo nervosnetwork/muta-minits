e4bbae013de7314c6de9d2144497712f50a94528
mohanson <mohanson@outlook.com>
2019-08-21 10:50:53 +0800

    Implement Do-while

---
+++ src/codegen/do-statement.ts
@@ -0,0 +1,47 @@
+import llvm from 'llvm-node';
+import ts from 'typescript';
+
+import LLVMCodeGen from './';
+
+export default class CodeGenDo {
+  private cgen: LLVMCodeGen;
+
+  constructor(cgen: LLVMCodeGen) {
+    this.cgen = cgen;
+  }
+
+  public genDoStatement(node: ts.DoStatement): void {
+    const loopBody = llvm.BasicBlock.create(
+      this.cgen.context,
+      'loop.body',
+      this.cgen.currentFunction
+    );
+    const loopCond = llvm.BasicBlock.create(
+      this.cgen.context,
+      'loop.cond',
+      this.cgen.currentFunction
+    );
+    const loopQuit = llvm.BasicBlock.create(
+      this.cgen.context,
+      'loop.quit',
+      this.cgen.currentFunction
+    );
+
+    this.cgen.builder.createBr(loopBody);
+    this.cgen.builder.setInsertionPoint(loopBody);
+    const rawBreakBlock = this.cgen.currentBreakBlock;
+    const rawContinueBlock = this.cgen.currentConitnueBlock;
+    this.cgen.currentBreakBlock = loopQuit;
+    this.cgen.currentConitnueBlock = loopCond;
+    this.cgen.genStatement(node.statement);
+    this.cgen.builder.createBr(loopCond);
+
+    this.cgen.builder.setInsertionPoint(loopCond);
+    const cond = this.cgen.genExpression(node.expression);
+    this.cgen.builder.createCondBr(cond, loopBody, loopQuit);
+
+    this.cgen.builder.setInsertionPoint(loopQuit);
+    this.cgen.currentBreakBlock = rawBreakBlock;
+    this.cgen.currentConitnueBlock = rawContinueBlock;
+  }
+}
---
+++ tests/ts/do.ts
@@ -0,0 +1,9 @@
+function main(): number {
+    let s = 0;
+    let i = 0;
+    do {
+        s += i;
+        i += 1;
+    } while (i <= 10)
+    return s; // 55
+}
