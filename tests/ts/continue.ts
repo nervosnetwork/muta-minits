635cec96b770de52e3bfa7783dde1bff00b33b0a
mohanson <mohanson@outlook.com>
2019-08-21 10:32:11 +0800

    Fix

--- tests/ts/continue.ts
+++ tests/ts/continue.ts
@@ -1,21 +1,21 @@
 function main(): number {
     let s = 0;
 
-    // for (let i = 0; i < 10; i++) {
-    //     if (i === 5) {
-    //         continue
-    //     }
-    //     s++;
-    // } // s === 9
+    for (let i = 0; i < 10; i++) {
+        if (i === 5) {
+            continue
+        }
+        s++;
+    } // s === 9
 
-    // let i = 0;
-    // while (i < 10) {
-    //     i++;
-    //     if (i === 5) {
-    //         continue
-    //     }
-    //     s++;
-    // } // s === 18
+    let i = 0;
+    while (i < 10) {
+        i++;
+        if (i === 5) {
+            continue
+        }
+        s++;
+    } // s === 18
 
     let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     for (let v of a) {
