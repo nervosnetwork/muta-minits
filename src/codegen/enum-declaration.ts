bb3c72d026f540d391732e98bcee2969e8116936
Mohanson <mohanson@outlook.com>
2019-09-03 14:45:05 +0800

    Rewrite enum with type checker (#43)

--- src/codegen/enum-declaration.ts
+++ src/codegen/enum-declaration.ts
@@ -1,12 +1,7 @@
-import Debug from 'debug';
 import llvm from 'llvm-node';
 import ts from 'typescript';
 
 import LLVMCodeGen from './';
-
-const debug = Debug('minits:codegen:enum');
-
-debug('codegen-enum');
 
 export default class CodeGenStruct {
   private readonly cgen: LLVMCodeGen;
@@ -16,42 +11,25 @@ export default class CodeGenStruct {
   }
 
   public genEnumDeclaration(node: ts.EnumDeclaration): void {
-    if (node.members.length === 0) {
-      return;
-    }
     this.cgen.symtab.with(node.name.text, () => {
-      this.genElements(node.members);
-    });
-  }
-
-  private genElements(members: ts.NodeArray<ts.EnumMember>): void {
-    // String
-    if (members[0].initializer && members[0].initializer.kind === ts.SyntaxKind.StringLiteral) {
-      for (const [, m] of members.entries()) {
-        const fieldName = m.name.getText();
-        const value = this.cgen.cgString.genStringLiteral(m.initializer as ts.StringLiteral);
-        this.cgen.symtab.set(fieldName, { inner: value, deref: 0 });
-      }
-      return;
-    }
-    // Number
-    let currentNum = 0;
-    for (const [index, m] of members.entries()) {
-      const fieldName = m.name.getText();
-      const value = (() => {
-        if (m.initializer) {
-          const v = this.cgen.cgNumeric.genNumeric(m.initializer as ts.NumericLiteral);
-          currentNum = v.value;
-          return v;
-        } else if (index === 0) {
-          return llvm.ConstantInt.get(this.cgen.context, 0, 64, true);
-        } else {
-          currentNum += 1;
-          return llvm.ConstantInt.get(this.cgen.context, currentNum, 64, true);
+      node.members.forEach(item => {
+        const name = item.name.getText();
+        const v = this.cgen.checker.getConstantValue(item);
+        switch (typeof v) {
+          case 'string':
+            this.cgen.symtab.set(name, {
+              deref: 0,
+              inner: this.cgen.cgString.genStringLiteral(item.initializer! as ts.StringLiteral)
+            });
+            break;
+          case 'number':
+            this.cgen.symtab.set(name, {
+              deref: 0,
+              inner: llvm.ConstantInt.get(this.cgen.context, v, 64)
+            });
+            break;
         }
-      })();
-      this.cgen.symtab.set(fieldName, { inner: value, deref: 0 });
-    }
-    return;
+      });
+    });
   }
 }
