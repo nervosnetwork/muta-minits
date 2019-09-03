32493d5be84afa41e17bb5d7b55631a9d144f889
mohanson <mohanson@outlook.com>
2019-09-03 18:12:13 +0800

    Fix the linesep in source code

--- src/tests/args.spec.ts
+++ src/tests/args.spec.ts
@@ -4,9 +4,11 @@ import { compileToLLVMIR } from './util';
 
 const srcArgsFromShell = `
 function main(argc: number, argv: string[]): number {
-    console.log('%s\n', argv[0]);
-    return 0;
-  }
+  console.log('%s\\n', argv[1]);
+  console.log('%s\\n', argv[2]);
+  console.log('%d\\n', argc);
+  return 0;
+}
 `;
 
 test('test args', async t => {
@@ -14,7 +16,6 @@ test('test args', async t => {
   const name = irPath.slice(0, irPath.length - 3);
 
   shell.exec(`llvm-as ${name}.ll -o ${name}.bc`, { async: false });
-  //   shell.exec(`llc ${name}.bc -o ${name}.s`, { async: false });
   shell.exec(`llc -filetype=obj ${name}.bc -o ${name}.o`, { async: false });
   shell.exec(`gcc ${name}.o -o ${name}`, { async: false });
   const ret = shell.exec(`${name} foo bar`, { async: false });
