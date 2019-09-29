bf8a71172dcb063c542927a37873f2cc110bf7c3
mohanson <mohanson@outlook.com>
2019-09-29 18:26:05 +0800

    2019-09-29 18:26:05

--- src/codegen/switch-statement.ts
+++ src/codegen/switch-statement.ts
@@ -12,17 +12,22 @@ export default class CodeGenSwitch {
   public genSwitchStatement(node: ts.SwitchStatement): void {
     const caseArr = [...node.caseBlock.clauses];
     const genNode = (v: ts.CaseOrDefaultClause, i: number, a: ts.CaseOrDefaultClause[]): ts.Statement => {
+      // Remove the break statement
+      const statements = v.statements.filter(s => {
+        return s.kind !== ts.SyntaxKind.BreakStatement;
+      });
+
       if (v.kind === ts.SyntaxKind.CaseClause) {
         return ts.createIf(
           ts.createBinary(node.expression, ts.createToken(ts.SyntaxKind.EqualsEqualsToken), v.expression),
-          ts.createBlock(v.statements, true),
+          ts.createBlock(statements, true),
           ts.createBlock([genNode(a[i + 1], i + 1, a)], true)
         );
       }
       if (v.kind === ts.SyntaxKind.DefaultClause) {
-        return ts.createBlock(v.statements, true);
+        return ts.createBlock(statements, true);
       }
-      throw new Error(''); // Should never reach this
+      throw new Error('');
     };
 
     this.cgen.genStatement(genNode(caseArr[0], 0, caseArr));
--- src/tests/switch-statement.spec.ts
+++ src/tests/switch-statement.spec.ts
@@ -3,25 +3,58 @@ import { runCode } from './util';
 
 const srcSwitchCaseDefault = `
 function main(): number {
-    let a = 2;
-    let r = 0;
+  let a = 2;
+  let r = 0;
 
-    switch (a) {
-        case 0:
-        r = 0;
-        case 1:
-        r = 1;
-        case 2:
-        r = 2;
-        default:
-        r = 3;
-    }
+  switch (a) {
+    case 0:
+      r = 0;
+      break;
+    case 1:
+      r = 1;
+      break;
+    case 2:
+      r = 2;
+      break;
+    default:
+      r = 3;
+      break;
+  }
 
-    return r;
+  return r;
 }
 `;
 
 test('test switch case default', async t => {
   await runCode(srcSwitchCaseDefault);
+  t.pass();
+});
+
+const srcSwitchString = `
+function main(): number {
+  let a = "B";
+  let r = 0;
+
+  switch (a) {
+    case "A":
+      r = 0;
+      break;
+    case "B":
+      r = 1;
+      break;
+    case "C":
+      r = 2;
+      break;
+    default:
+      r = 3;
+      break;
+  }
+
+  return r;
+}
+`;
+
+test('test switch string', async t => {
+  await runCode(srcSwitchString);
   t.pass();
 });
